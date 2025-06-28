a4a0b3bedb9e8a4e7ff3e0e25b2636372d6410cc
Ramesh <40071878+rsusarla@users.noreply.github.com>
2025-06-28 14:51:22 -0400

    Update charges_prediction.py

--- charges_prediction.py
+++ charges_prediction.py
@@ -4,8 +4,8 @@ import numpy as np
 import joblib
 
 model=joblib.load('linear_regression_model.joblib')
-
-st.title("Insurance Charges Prediction - RS Insurance")
+st.title("*****  RS Insurance  *****")
+st.title("Insurance Charges Prediction")
 st.subheader("Enter the details to predict the insurance charges")
 
 claim_amount=st.number_input("Enter the claim amount: ",min_value=0.0,format="%.2f")
