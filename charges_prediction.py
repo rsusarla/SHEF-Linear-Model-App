505a23a9903a53dea457eeacd357b040a603beb9
Ramesh <40071878+rsusarla@users.noreply.github.com>
2025-06-28 15:15:46 -0400

    Update charges_prediction.py

--- charges_prediction.py
+++ charges_prediction.py
@@ -4,8 +4,8 @@ import joblib
 
 model=joblib.load('linear_regression_model.joblib')
 
-st.header("******* RS Insurance ******* ")
-st.title("Insurance Charges Prediction")
+st.title("******* RS Insurance ******* ")
+st.header("Insurance Charges Prediction")
 
 st.subheader("Enter the details to predict the insurance charges")
 
