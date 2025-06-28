10aa938a75d97574efa31c08354531d06cfe91c6
Ramesh <40071878+rsusarla@users.noreply.github.com>
2025-06-28 17:35:22 -0400

    Update charges_prediction.py

--- charges_prediction.py
+++ charges_prediction.py
@@ -5,8 +5,7 @@ import joblib
 model=joblib.load('linear_regression_model.joblib')
 
 #st.title("********   RS Insurance   ******** ")
-st.markdown("<h1 style='color:blue;'>********   RS Insurance   ********</h1>", unsafe_allow_html=True
-
+st.markdown("<h1 style='color:blue;'>********   RS Insurance   ********</h1>", unsafe_allow_html=True)
 st.header("   Insurance Charges Prediction   ")
 
 st.subheader("Enter the details to predict the insurance charges")
